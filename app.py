2527999616c7e1f8f72625369b6df90bf48da4df
thanhnt27022000 <thanhnt27022000@gmail.com>
2022-11-10 15:12:03 +0700

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 
 app = Flask(__name__) #create an instance of the Flask library
 
-@app.route('/hello') #whenever this webserver is called with <hostname:port>/hello then this section is called
+@app.route('/') #whenever this webserver is called with <hostname:port>/hello then this section is called
 def hello(): #The subroutine name that handles the call
 	output = 'Hello World'
 	return output #Whatever is returned from this subroutine is what is returned to the requester and is shown on the browser page
